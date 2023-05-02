cae082a73067d5a3358c60cd862c1aa42e25202a
James Marnie Argarin <jnargarin@up.edu.ph>
2023-05-02 20:14:53 +0800

    feat: Modified doctors list page UI

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -35,6 +35,24 @@ class HomeScreen extends HookConsumerWidget {
         toFirestore: (doctor, _) => doctor.toMap(),
       );
 
+  TabBar tabBar(TabController tabController) => TabBar(
+        controller: tabController,
+        tabs: const [
+          Tab(
+            text: 'Patients',
+            icon: Icon(Icons.person),
+          ),
+          Tab(
+            text: 'Doctors',
+            icon: Icon(Icons.person),
+          ),
+          Tab(
+            text: 'Blocked',
+            icon: Icon(Icons.person),
+          )
+        ],
+      );
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final FirestoreService firestoreService = FirestoreService();
@@ -53,23 +71,7 @@ class HomeScreen extends HookConsumerWidget {
               },
               icon: const Icon(Icons.logout))
         ],
-        bottom: TabBar(
-          controller: tabController,
-          tabs: const [
-            Tab(
-              text: 'Patients',
-              icon: Icon(Icons.person),
-            ),
-            Tab(
-              text: 'Doctors',
-              icon: Icon(Icons.person),
-            ),
-            Tab(
-              text: 'Blocked',
-              icon: Icon(Icons.person),
-            )
-          ],
-        ),
+        bottom: tabBar(tabController),
       ),
       body: Background(
         child: TabBarView(
@@ -235,68 +237,165 @@ class HomeScreen extends HookConsumerWidget {
                 ),
               ),
             ),
-            StreamBuilder(
-              stream: firestoreService.getUsersSnapshots(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.hasData) {
-                  return ListView(
-                    children: snapshot.data!.docs
-                        .where((QueryDocumentSnapshot snapshot) =>
-                            snapshot[ModelFields.isApproved])
-                        .map(
-                      (QueryDocumentSnapshot snapshot) {
-                        bool isPatient =
-                            snapshot[ModelFields.role].toLowerCase() ==
-                                AppConstants.patient.toLowerCase();
+            Center(
+              child: Container(
+                height: 560.h,
+                width: isWeb(maxWidth) ? 140.w : 340.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.r),
+                  color: ColorConstants.primaryLight,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: isWeb(maxWidth) ? 135.w : 320.w,
+                      height: 60.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.r),
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'List of Doctors',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: isWeb(maxWidth) ? 7.sp : 18.sp),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Flexible(
+                      child: Container(
+                        width: isWeb(maxWidth) ? 135.w : 320.w,
+                        height: 470.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: Colors.white,
+                        ),
+                        child: ScrollConfiguration(
+                          behavior: ScrollConfiguration.of(context).copyWith(
+                            dragDevices: {
+                              PointerDeviceKind.touch,
+                              PointerDeviceKind.mouse,
+                            },
+                          ),
+                          child: FirestoreQueryBuilder(
+                            query: doctorsQuery,
+                            builder: (context, snapshot, _) {
+                              if (snapshot.hasError) {
+                                return Center(
+                                  child: Text(
+                                    Prompts.connectionError,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            isWeb(maxWidth) ? 3.sp : 14.sp),
+                                  ),
+                                );
+                              }
 
-                        if (isPatient) {
-                          Patient patient = Patient.fromSnapshot(snapshot);
-                          return ListTile(
-                            onTap: () {},
-                            title: Center(
-                              child: Text(
-                                  '${patient.firstName} ${patient.lastName}'),
-                            ),
-                            subtitle: Center(
-                              child: Text(patient.role),
-                            ),
-                            trailing: IconButton(
-                              onPressed: () async {
-                                await firebaseServicesNotifier
-                                    .approveRegistration(patient.id, false);
-                              },
-                              icon: const Icon(Icons.close),
-                            ),
-                          );
-                        } else {
-                          Doctor doctor = Doctor.fromSnapshot(snapshot);
-                          return ListTile(
-                            onTap: () {},
-                            title: Center(
-                              child: Text(
-                                  '${doctor.firstName} ${doctor.lastName}'),
-                            ),
-                            subtitle: Center(
-                              child: Text(doctor.role),
-                            ),
-                            trailing: IconButton(
-                              onPressed: () async {
-                                await firebaseServicesNotifier
-                                    .approveRegistration(doctor.id, false);
-                              },
-                              icon: const Icon(Icons.close),
-                            ),
-                          );
-                        }
-                      },
-                    ).toList(),
-                  );
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+                              if (snapshot.hasData) {
+                                return snapshot.docs.isEmpty
+                                    ? Center(
+                                        child: Text(
+                                          Prompts.noPatients,
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: isWeb(maxWidth)
+                                                  ? 3.sp
+                                                  : 14.sp),
+                                        ),
+                                      )
+                                    : ListView.builder(
+                                        itemCount: snapshot.docs.length,
+                                        itemBuilder: (context, index) {
+                                          if (snapshot.hasMore &&
+                                              index + 1 ==
+                                                  snapshot.docs.length) {
+                                            snapshot.fetchMore();
+                                          }
+
+                                          final Doctor doctor =
+                                              snapshot.docs[index].data();
+
+                                          return Padding(
+                                            padding: EdgeInsets.symmetric(
+                                                vertical: 5.h,
+                                                horizontal: 10.w),
+                                            child: Material(
+                                              type: MaterialType.transparency,
+                                              child: ListTile(
+                                                tileColor: Colors.grey[200],
+                                                shape: RoundedRectangleBorder(
+                                                  side: BorderSide(
+                                                      color: Colors.grey[300]!),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10.r),
+                                                ),
+                                                onTap: null,
+                                                title: Center(
+                                                  child: Text(
+                                                      '${doctor.prefix} ${doctor.firstName} ${doctor.lastName} ${doctor.suffix}'
+                                                          .trim(),
+                                                      style: TextStyle(
+                                                          fontSize:
+                                                              isWeb(maxWidth)
+                                                                  ? 5.sp
+                                                                  : 12.sp,
+                                                          fontWeight:
+                                                              FontWeight.bold)),
+                                                ),
+                                                subtitle: Center(
+                                                  child: Text(
+                                                      'Email: ${doctor.email}',
+                                                      style: TextStyle(
+                                                          fontSize:
+                                                              isWeb(maxWidth)
+                                                                  ? 4.sp
+                                                                  : 10.sp)),
+                                                ),
+                                                trailing: Row(
+                                                  mainAxisSize:
+                                                      MainAxisSize.min,
+                                                  children: <Widget>[
+                                                    Center(
+                                                      child: IconButton(
+                                                        onPressed: () async {
+                                                          await firebaseServicesNotifier
+                                                              .approveRegistration(
+                                                                  doctor.id,
+                                                                  false);
+                                                        },
+                                                        icon: const Icon(
+                                                          Icons.close,
+                                                          color: Colors.red,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      );
+                              }
+
+                              return lottieLoading(
+                                  width: isWeb(maxWidth) ? 10 : 50);
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             StreamBuilder(
               stream: firestoreService.getUsersSnapshots(),
